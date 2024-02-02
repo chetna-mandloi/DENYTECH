27149e367bcd873a2b039757f4dd2df92edf787e
chetna-mandloi <158042058+chetna-mandloi@users.noreply.github.com>
2024-02-02 10:58:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+gsap.from("nav",{
+   y:-30,
+   // x:1000,
+   duration:1,
+   delay:1.5,
+   opacity:0,
+   // stagger:7,
+   // repeat:-0
+})
+
+    
+
+gsap.from("a",{
+    y:-59,
+    duration:2,
+    delay: 1.5,
+    opacity:1,
+    stagger:0.3,
+    repeat:1
+ 
+   })
+
+   gsap.from("i",{
+      y:-59,
+      duration:2,
+      delay: 1.5,
+      opacity:1,
+      stagger:0.3,
+      repeat:1
+   
+     })
+  
+  
+  
+
+
+
+
+     
+ 
